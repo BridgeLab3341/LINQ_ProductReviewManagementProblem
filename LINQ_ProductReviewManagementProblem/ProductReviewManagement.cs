d27aba36335f84ec48d4f3eac768684e20020fce
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-14 08:57:45 +0530

    [somu]Add:Retrieve Top 3 Records from list of Products.

---
+++ LINQ_ProductReviewManagementProblem/ProductReviewManagement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_ProductReviewManagementProblem
+{
+    public class ProductReviewManagement
+    {
+        public void RetrieveTop3Records(List<ProductsReview> list)
+        {
+            //var top3=list.Where(x => x.Rating ==3).ToList();
+            var top3 = (from ProductsReview in list
+                        orderby ProductsReview.Rating descending
+                        select ProductsReview).Take(3);
+            Console.WriteLine("Top 3 Records are \n=====================================================");
+            foreach (var data in top3)
+            {
+                Console.WriteLine($"ProductID:- {data.ProductId} \t UserID:- {data.UserId} \t Rating:- {data.Rating} " +$"\t Review:- {data.Review} \t IsLike:- {data.IsLike}");
+            }
+        }
+    }
+}
--- LINQ_ProductReviewManagementProblem/Program.cs
+++ LINQ_ProductReviewManagementProblem/Program.cs
@@ -7,36 +7,54 @@ namespace LINQ_ProductReviewManagementProblem
         public static void Main(string[] args)
         {
             List<ProductsReview> products = new List<ProductsReview>();
-            products.Add(new ProductsReview { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 2, UserId = 5, Rating = 4, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 3, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 4, UserId = 7, Rating = 3, Review = "Average", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 5, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 6, UserId = 3, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 7, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 8, UserId = 6, Rating = 4, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 9, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 10, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 11, UserId = 6, Rating = 3, Review = "Average", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 12, UserId = 8, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 13, UserId = 2, Rating = 4, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 14, UserId = 4, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 15, UserId = 6, Rating = 2, Review = "BAverage", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 16, UserId = 5, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 17, UserId = 9, Rating = 4, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 18, UserId = 11, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 19, UserId = 14, Rating = 3, Review = "Average", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 20, UserId = 1, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 21, UserId = 16, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 22, UserId = 1, Rating = 1, Review = "Worst", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 23, UserId = 10, Rating = 2, Review = "BAverage", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 24, UserId = 11, Rating = 5, Review = "Good", IsLike = true });
-            products.Add(new ProductsReview { ProductId = 25, UserId = 12, Rating = 4, Review = "Good", IsLike = true });
-            Console.WriteLine("Product Deatils given in Table");
-            Console.WriteLine("------------------------------");
-            foreach (var data in products)
+            ProductReviewManagement review = new ProductReviewManagement();
+            bool flag = true;
+            while (flag)
             {
-                Console.WriteLine(data.ProductId + "  " + data.UserId + "  " + data.Rating + "  " + data.Review + "  " + data.IsLike);
+                Console.WriteLine("Choose Option to Perform the Operation\n1.Insert 25 Records\n2.Retrieve Top 3 Records\n3.Exit");
+                int option=Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        products.Add(new ProductsReview { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 2, UserId = 5, Rating = 4, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 3, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 4, UserId = 7, Rating = 3, Review = "Average", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 5, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 6, UserId = 3, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 7, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 8, UserId = 6, Rating = 4, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 9, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 10, UserId = 1, Rating = 5, Review = "Worst", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 11, UserId = 6, Rating = 3, Review = "Average", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 12, UserId = 8, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 13, UserId = 2, Rating = 4, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 14, UserId = 4, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 15, UserId = 6, Rating = 2, Review = "BAverage", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 16, UserId = 5, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 17, UserId = 9, Rating = 4, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 18, UserId = 11, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 19, UserId = 14, Rating = 3, Review = "Average", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 20, UserId = 1, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 21, UserId = 16, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 22, UserId = 1, Rating = 1, Review = "Worst", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 23, UserId = 10, Rating = 2, Review = "BAverage", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 24, UserId = 11, Rating = 5, Review = "Good", IsLike = true });
+                        products.Add(new ProductsReview { ProductId = 25, UserId = 12, Rating = 4, Review = "Good", IsLike = true });
+                        Console.WriteLine("Product Details given in Table");
+                        Console.WriteLine("------------------------------");
+                        foreach (var data in products)
+                        {
+                            Console.WriteLine(data.ProductId + "  " + data.UserId + "  " + data.Rating + "  " + data.Review + "  " + data.IsLike);
+                        }
+                        break;
+                        case 2:
+                        review.RetrieveTop3Records(products);
+                        break;
+                        case 3:
+                        flag= false;
+                        break;
+                }
             }
         }
     }
